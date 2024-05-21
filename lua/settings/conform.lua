b0afe7eed58b3a0ea3b984dc9d166187d23d5b52
Matheus Lima <matheus.limastack@outlook.com>
2024-05-21 07:01:19 -0300

    add stylua to comform

--- lua/settings/conform.lua
+++ lua/settings/conform.lua
@@ -1,14 +1,15 @@
 vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*",
-  callback = function(args)
-    require("conform").format({ bufnr = args.buf })
-  end,
+	pattern = "*",
+	callback = function(args)
+		require("conform").format({ bufnr = args.buf })
+	end,
 })
 
 require("conform").setup({
-  formatters_by_ft = {
-    javascript = { { "prettier", "prettierd" } },
-    typescript = { { "prettier", "prettierd" } },
-  },
+	formatters_by_ft = {
+		javascript = { { "prettier", "prettierd" } },
+		typescript = { { "prettier", "prettierd" } },
+		ruby = { { "rubocop" } },
 		lua = { { "stylua", "stylua" } },
+	},
 })
