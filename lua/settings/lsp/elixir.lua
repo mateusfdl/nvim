05055eb5802770214ab3d2e9c2085e0c7187981b
Matheus Lima <matheus.limastack@outlook.com>
2024-05-21 06:59:21 -0300

    add elixir lsp config

---
+++ lua/settings/lsp/elixir.lua
@@ -0,0 +1,10 @@
+local M = {}
+
+function M.attacher(client)
+  client.resolved_capabilities.document_formatting = false
+  client.resolved_capabilities.document_range_formatting = false
+end
+
+M.cmd = { "elixir-ls" }
+
+return M
