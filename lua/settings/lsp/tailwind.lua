2e4bd26480db9f70b2e83a6fd03fa7ffa3962f41
Matheus Lima <matheus.limastack@outlook.com>
2024-07-16 13:55:18 -0300

    add tailwind lsp

---
+++ lua/settings/lsp/tailwind.lua
@@ -0,0 +1,51 @@
+local M = {}
+
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+capabilities.textDocument.colorProvider = { dynamicRegistration = false }
+capabilities.textDocument.foldingRange = {
+	dynamicRegistration = false,
+	lineFoldingOnly = true,
+}
+M.capabilities = capabilities
+
+M.on_attach = function(client, bufnr)
+	if client.server_capabilities.colorProvider then
+		require("utils.document-colors").buf_attach(bufnr)
+		require("colorizer").attach_to_buffer(
+			bufnr,
+			{ mode = "background", css = true, names = false, tailwind = false }
+		)
+	end
+end
+
+M.settings = {
+	tailwindCSS = {
+		lint = {
+			cssConflict = "warning",
+			invalidApply = "error",
+			invalidConfigPath = "error",
+			invalidScreen = "error",
+			invalidTailwindDirective = "error",
+			invalidVariant = "error",
+			recommendedVariantOrder = "warning",
+		},
+		experimental = {
+			classRegex = {
+				"tw`([^`]*)",
+				'tw="([^"]*)',
+				'tw={"([^"}]*)',
+				"tw\\.\\w+`([^`]*)",
+				"tw\\(.*?\\)`([^`]*)",
+				{ "clsx\\(([^)]*)\\)", "(?:'|\"|`)([^']*)(?:'|\"|`)" },
+				{ "classnames\\(([^)]*)\\)", "'([^']*)'" },
+				{ "cva\\(([^)]*)\\)", "[\"'`]([^\"'`]*).*?[\"'`]" },
+				{ "cn\\(([^)]*)\\)", "(?:'|\"|`)([^']*)(?:'|\"|`)" },
+			},
+		},
+		validate = true,
+	},
+}
+M.filetypes = { "html", "mdx", "javascriptreact", "typescriptreact", "vue", "svelte" }
+
+return M
