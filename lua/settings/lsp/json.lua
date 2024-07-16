96ab73fa3e10def77f5a7b17ea2c51041fc9fd12
Matheus Lima <matheus.limastack@outlook.com>
2024-07-16 13:55:14 -0300

    add json lsp

---
+++ lua/settings/lsp/json.lua
@@ -0,0 +1,50 @@
+local M = {}
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+M.capabilities = capabilities
+
+M.settings = {
+	json = {
+		schemas = {
+			{
+				fileMatch = { "package.json" },
+				url = "https://json.schemastore.org/package.json",
+			},
+			{
+				fileMatch = { "tsconfig*.json" },
+				url = "https://json.schemastore.org/tsconfig.json",
+			},
+			{
+				fileMatch = { ".prettierrc", ".prettierrc.json", "prettier.config.json" },
+				url = "https://json.schemastore.org/prettierrc.json",
+			},
+			{
+				fileMatch = { ".eslintrc", ".eslintrc.json" },
+				url = "https://json.schemastore.org/eslintrc.json",
+			},
+			{
+				fileMatch = { ".babelrc", ".babelrc.json", "babel.config.json" },
+				url = "https://json.schemastore.org/babelrc.json",
+			},
+			{
+				fileMatch = { "lerna.json" },
+				url = "https://json.schemastore.org/lerna.json",
+			},
+			{
+				fileMatch = { "now.json", "vercel.json" },
+				url = "https://json.schemastore.org/now.json",
+			},
+			{
+				fileMatch = { "ecosystem.json" },
+				url = "https://json.schemastore.org/pm2-ecosystem.json",
+			},
+			{
+				fileMatch = { "nest-cli.json" },
+				url = "https://json.schemastore.org/nest-cli",
+			},
+		},
+	},
+}
+
+return M
