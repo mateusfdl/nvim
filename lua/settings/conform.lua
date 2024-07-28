c35dcf0f4248a46578c70206221fde675ec71d92
Matheus Lima <matheus.limastack@outlook.com>
2024-07-28 12:28:21 -0300

    check which formatter should be applied based on project setting

--- lua/settings/conform.lua
+++ lua/settings/conform.lua
@@ -1,15 +1,23 @@
+require("conform").setup({
+	formatters_by_ft = {
+		typescript = function(bufnr)
+			local path = vim.fn.getcwd()
+
+			if vim.fs.find("biome.json", { upward = true, path = path })[1] then
+				return { "biomejs" }
+			else
+				return { "prettier" }
+			end
+		end,
+		javascript = {},
+		ruby = { { "rubocop" } },
+		lua = { { "stylua", "stylua" } },
+	},
+})
+
 vim.api.nvim_create_autocmd("BufWritePre", {
 	pattern = "*",
 	callback = function(args)
 		require("conform").format({ bufnr = args.buf })
 	end,
-})
-
-require("conform").setup({
-	formatters_by_ft = {
-		javascript = { { "prettier", "prettierd", "biome" } },
-		typescript = { { "prettier", "prettierd", "biome" } },
-		ruby = { { "rubocop" } },
-		lua = { { "stylua", "stylua" } },
-	},
 })
