3a04a57722255a556d57fad9802f1b5428249c7d
Matheus Lima <matheus.limastack@outlook.com>
2024-12-13 22:07:35 -0300

    lazy load lsp configs

--- lua/settings/lsp.lua
+++ lua/settings/lsp.lua
@@ -1,41 +1,41 @@
-local capabilities = require("cmp_nvim_lsp").default_capabilities()
+vim.api.nvim_create_autocmd("VimEnter", {
+	callback = function()
+		local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-local ruby = require("settings.lsp.solargraph")
-local ts = require("settings.lsp.tsserver")
-local go = require("settings.lsp.golang")
-local haskell = require("settings.lsp.haskell")
-local lua = require("settings.lsp.lua")
-local c = require("settings.lsp.clangd")
-local elixir = require("settings.lsp.elixir")
-local json = require("settings.lsp.json")
-local tailwind = require("settings.lsp.tailwind")
-local sourcekit = require("settings.lsp.sourcekit")
+		local ruby = require("settings.lsp.solargraph")
+		local ts = require("settings.lsp.tsserver")
+		local go = require("settings.lsp.golang")
+		local haskell = require("settings.lsp.haskell")
+		local lua = require("settings.lsp.lua")
+		local c = require("settings.lsp.clangd")
+		local elixir = require("settings.lsp.elixir")
+		local json = require("settings.lsp.json")
+		local tailwind = require("settings.lsp.tailwind")
+		local sourcekit = require("settings.lsp.sourcekit")
 
-local M = {}
+		local servers = {
+			ts_ls = ts,
+			solargraph = ruby,
+			gopls = go,
+			hls = haskell,
+			lua_ls = lua,
+			clangd = c,
+			elixirls = elixir,
+			jsonls = json,
+			tailwindcss = tailwind,
+			sourcekit = sourcekit,
+		}
 
-M.servers = {
-	ts_ls = ts,
-	solargraph = ruby,
-	gopls = go,
-	hls = haskell,
-	lua_ls = lua,
-	clangd = c,
-	elixirls = elixir,
-	jsonls = json,
-	tailwindcss = tailwind,
-	sourcekit = sourcekit,
-}
-
-function M.setup_servers()
-	for lsp, config in pairs(M.servers) do
-		require("lspconfig")[lsp].setup({
-			capabilities = capabilities,
-			on_attach = config.on_attach,
-			cmd = config.cmd,
-			settings = config.settings,
-			filetypes = config.filetypes,
-		})
-	end
-end
-
-return M
+		for lsp, config in pairs(servers) do
+			vim.schedule(function()
+				require("lspconfig")[lsp].setup({
+					capabilities = capabilities,
+					on_attach = config.on_attach,
+					cmd = config.cmd,
+					settings = config.settings,
+					filetypes = config.filetypes,
+				})
+			end)
+		end
+	end,
+})
