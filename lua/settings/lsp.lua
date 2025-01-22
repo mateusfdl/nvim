ec0d64f606cd0826d4a0c7ca6846477bda4eaa5f
Matheus Lima <matheus.limastack@outlook.com>
2025-01-22 11:13:41 -0300

    setup LSP clients on VimEnter aucmd

--- lua/settings/lsp.lua
+++ lua/settings/lsp.lua
@@ -1,44 +1,42 @@
-local lsp_clients = {}
+local M = {}
+M.lsp_clients = {}
 
-vim.api.nvim_create_autocmd("BufEnter", {
-    callback = function()
-        local buf = vim.api.nvim_get_current_buf()
-        local buf_name = vim.api.nvim_buf_get_name(buf)
+local async = require("plenary.async")
 
-        -- Skip [No Name] buffers and buffers already initialized
-        if buf_name == "" or lsp_clients[buf] then
-            return
-        end
+function M.setup_lsp_servers()
+	local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-        local capabilities = require("cmp_nvim_lsp").default_capabilities()
+	local lsp_configs = {
+		ts_ls = require("settings.lsp.ts_ls"),
+		solargraph = require("settings.lsp.solargraph"),
+		gopls = require("settings.lsp.golang"),
+		hls = require("settings.lsp.haskell"),
+		lua_ls = require("settings.lsp.lua"),
+		clangd = require("settings.lsp.clangd"),
+		elixirls = require("settings.lsp.elixir"),
+		jsonls = require("settings.lsp.json"),
+		tailwindcss = require("settings.lsp.tailwind"),
+		sourcekit = require("settings.lsp.sourcekit"),
+	}
 
-        local lsp_configs = {
-            ts_ls = require("settings.lsp.ts_ls"),
-            solargraph = require("settings.lsp.solargraph"),
-            gopls = require("settings.lsp.golang"),
-            hls = require("settings.lsp.haskell"),
-            lua_ls = require("settings.lsp.lua"),
-            clangd = require("settings.lsp.clangd"),
-            elixirls = require("settings.lsp.elixir"),
-            jsonls = require("settings.lsp.json"),
-            tailwindcss = require("settings.lsp.tailwind"),
-            sourcekit = require("settings.lsp.sourcekit"),
-        }
+	for lsp, config in pairs(lsp_configs) do
+		async.run(function()
+			require("lspconfig")[lsp].setup({
+				capabilities = capabilities,
+				on_attach = config.on_attach,
+				cmd = config.cmd,
+				settings = config.settings,
+				filetypes = config.filetypes,
+				init_options = config.init_options,
+			})
+		end)
+	end
+end
 
-        for lsp, config in pairs(lsp_configs) do
-            vim.schedule(function()
-                require("lspconfig")[lsp].setup({
-                    capabilities = capabilities,
-                    on_attach = config.on_attach,
-                    cmd = config.cmd,
-                    settings = config.settings,
-                    filetypes = config.filetypes,
-                    init_options = config.init_options,
-                })
-            end)
-        end
-
-        -- Mark the buffer as initialized
-        lsp_clients[buf] = true
-    end,
+vim.api.nvim_create_autocmd("VimEnter", {
+	callback = function()
+		async.run(M.setup_lsp_servers)
+	end,
 })
+
+return M
