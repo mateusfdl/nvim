2cd978b3615e43c7dd7460e8493c89b1f8246ef3
Matheus Lima <matheus.limastack@outlook.com>
2025-02-04 11:07:24 -0300

    add new debug functions

---
+++ lua/utils/debug.lua
@@ -0,0 +1,33 @@
+local log_file = "/tmp/nvim_log_" .. tostring(math.random(100000, 999999)) .. ".json"
+
+local function log_to_file(data)
+	vim.schedule(function()
+		local json = vim.fn.json_encode(data)
+		vim.fn.writefile({ json }, log_file, "a")
+	end)
+end
+
+P = function(v)
+	local info = debug.getinfo(2, "Sl")
+	local log_entry = {
+		value = v,
+		source = info.source,
+		line = info.currentline,
+	}
+	print(vim.inspect(v))
+	log_to_file(log_entry)
+	return v
+end
+
+P_nested_path = function(tbl, keys)
+	if #keys == 0 then
+		return M.P(tbl)
+	end
+	local key = keys[1]
+	if tbl[key] == nil then
+		return nil
+	end
+	return M.P_nested_path(tbl[key], { unpack(keys, 2) })
+end
+
+return M
