f8dc9eee2c67489f7df26b37c5aa683109a7dc26
Matheus Lima <matheus.limastack@gmail.com>
2025-03-25 10:52:19 -0300

    add schadenfreude new adapters

--- lua/settings/schadenfreude.lua
+++ lua/settings/schadenfreude.lua
@@ -6,6 +6,52 @@ require("schadenfreude").setup({
 		options = {
 			model = "llama-3.3-70b-versatile",
 			url = "https://api.groq.com/openai/v1/chat/completions",
+			debug = true,
+			debug_log_file = vim.fn.stdpath("data") .. "/schadenfreude_debug.log", -- Optional, custom log file path
+		},
+	},
+	{
+		provider = "deepseek",
+		interface = "openai",
+		api_key = vim.fn.getenv("GROQ_API_KEY"),
+		options = {
+			model = "deepseek-r1-distill-llama-70b-specdec",
+			url = "https://api.groq.com/openai/v1/chat/completions",
+			debug = true,
+			debug_log_file = vim.fn.stdpath("data") .. "/schadenfreude_debug.log", -- Optional, custom log file path
+		},
+	},
+	{
+		provider = "llama-specdec",
+		interface = "openai",
+		api_key = vim.fn.getenv("GROQ_API_KEY"),
+		options = {
+			model = "llama-3.3-70b-specdec",
+			url = "https://api.groq.com/openai/v1/chat/completions",
+			debug = true,
+			debug_log_file = vim.fn.stdpath("data") .. "/schadenfreude_debug.log", -- Optional, custom log file path
+		},
+	},
+	{
+		provider = "deepseek-v3",
+		interface = "openai",
+		api_key = vim.fn.getenv("OPEN_ROUTER_KEY"),
+		options = {
+			model = "deepseek/deepseek-chat-v3-0324:free",
+			url = "https://openrouter.ai/api/v1/chat/completions",
+			debug = true,
+			debug_log_file = vim.fn.stdpath("data") .. "/schadenfreude_debug.log", -- Optional, custom log file path
+		},
+	},
+	{
+		provider = "gemma",
+		interface = "openai",
+		api_key = vim.fn.getenv("OPEN_ROUTER_KEY"),
+		options = {
+			model = "google/gemini-2.0-flash-001",
+			url = "https://openrouter.ai/api/v1/chat/completions",
+			debug = true,
+			debug_log_file = vim.fn.stdpath("data") .. "/schadenfreude_debug.log", -- Optional, custom log file path
 		},
 	},
 	{
@@ -47,11 +93,11 @@ require("schadenfreude").setup({
 	{
 		provider = "local-qwen",
 		interface = "openai",
-		api_key = "",
+		api_key = "fooooo",
 		options = {
 			model = "qwen2.5-coder-14b-instruct",
 			url = "http://desktop:1234/v1/chat/completions",
 		},
 	},
-	selected_provider = "sonnet",
+	selected_provider = "llmama",
 })
