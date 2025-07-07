d2cf8bacab5904c48ea725d25533ea0097fa7e99
Matheus Lima <matheus.limastack@outlook.com>
2025-07-07 08:45:29 -0300

    removed unused models

--- lua/settings/schadenfreude.lua
+++ lua/settings/schadenfreude.lua
@@ -9,66 +9,12 @@ require("schadenfreude").setup({
 		},
 	},
 	{
-		provider = "deepseek",
-		interface = "openai",
-		api_key = vim.fn.getenv("GROQ_API_KEY"),
-		options = {
-			model = "deepseek-r1-distill-qwen-32b",
-			url = "https://api.groq.com/openai/v1/chat/completions",
-		},
-	},
-	{
-		provider = "llama-specdec",
-		interface = "openai",
-		api_key = vim.fn.getenv("GROQ_API_KEY"),
-		options = {
-			model = "llama-3.3-70b-specdec",
-			url = "https://api.groq.com/openai/v1/chat/completions",
-		},
-	},
-	{
-		provider = "deepseek-v3",
-		interface = "openai",
-		api_key = vim.fn.getenv("OPEN_ROUTER_KEY"),
-		options = {
-			model = "deepseek/deepseek-chat-v3-0324:free",
-			url = "https://openrouter.ai/api/v1/chat/completions",
-		},
-	},
-	{
 		provider = "gemma",
 		interface = "openai",
 		api_key = vim.fn.getenv("GEMINI"),
 		options = {
-			model = "gemini-2.0-flash",
+			model = "gemini-2.5-pro",
 			url = "https://generativelanguage.googleapis.com/v1beta/openai/chat/completions",
-		},
-	},
-	{
-		provider = "rich gemma",
-		interface = "openai",
-		api_key = vim.fn.getenv("GEMINI"),
-		options = {
-			model = "gemini-2.5-pro-preview-03-25",
-			url = "https://generativelanguage.googleapis.com/v1beta/openai/chat/completions",
-		},
-	},
-	{
-		provider = "qwen",
-		interface = "openai",
-		api_key = vim.fn.getenv("GROQ_API_KEY"),
-		options = {
-			model = "qwen-qwq-32b",
-			url = "https://api.groq.com/openai/v1/chat/completions",
-		},
-	},
-	{
-		provider = "mixtral",
-		interface = "openai",
-		api_key = vim.fn.getenv("GROQ_API_KEY"),
-		options = {
-			model = "mixtral-8x7b-32768",
-			url = "https://api.groq.com/openai/v1/chat/completions",
 		},
 	},
 	{
@@ -89,23 +35,5 @@ require("schadenfreude").setup({
 			url = "https://api.anthropic.com/v1/messages",
 		},
 	},
-	{
-		provider = "quasar",
-		interface = "openai",
-		api_key = vim.fn.getenv("OPEN_ROUTER_KEY"),
-		options = {
-			model = "openrouter/quasar-alpha",
-			url = "https://openrouter.ai/api/v1/chat/completions",
-		},
-	},
-	{
-		provider = "local-qwen",
-		interface = "openai",
-		api_key = "fooooo",
-		options = {
-			model = "qwen2.5-coder-14b-instruct",
-			url = "http://desktop:1234/v1/chat/completions",
-		},
-	},
-	selected_provider = "qwen",
+	selected_provider = "gemma",
 })
