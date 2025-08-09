a847fa4746cfc299ab83f73c49e92cecb6e09619
Matheus Lima <matheus.limastack@gmail.com>
2025-08-09 16:56:20 -0300

    add back go dap config

--- lua/settings/dap/go.lua
+++ lua/settings/dap/go.lua
@@ -11,46 +11,46 @@ dap.adapters.delve = {
 }
 
 dap_go.setup()
---
--- dap.configurations.go = {
--- 	{
--- 		type = "go",
--- 		name = "Debug Single File",
--- 		request = "launch",
--- 		program = "${file}",
--- 	},
--- 	{
--- 		type = "go",
--- 		name = "Debug Current Project",
--- 		request = "launch",
--- 		program = "./${relativeFileDirname}",
--- 	},
--- 	{
--- 		type = "go",
--- 		name = "Debug test", -- configuration for debugging test files
--- 		request = "launch",
--- 		mode = "test",
--- 		program = "${file}",
--- 	},
--- 	{
--- 		type = "go",
--- 		name = "Debug test (go.mod)",
--- 		request = "launch",
--- 		mode = "test",
--- 		program = "./${relativeFileDirname}",
--- 	},
--- 	{
--- 		type = "go",
--- 		name = "Debug Current CMD/main.go",
--- 		request = "launch",
--- 		program = "./cmd/app/main.go",
--- 	},
---   {
--- 		type = "go",
--- 		name = "Attach to Running Delve",
--- 		request = "attach",
--- 		mode = "remote",
--- 		port = 40000, -- Change this to match the port used by your running dlv instance
--- 		host = "127.0.0.1",
--- 	},
--- }
+
+dap.configurations.go = {
+	{
+		type = "go",
+		name = "Debug Single File",
+		request = "launch",
+		program = "${file}",
+	},
+	{
+		type = "go",
+		name = "Debug Current Project",
+		request = "launch",
+		program = "./${relativeFileDirname}",
+	},
+	{
+		type = "go",
+		name = "Debug test", -- configuration for debugging test files
+		request = "launch",
+		mode = "test",
+		program = "${file}",
+	},
+	{
+		type = "go",
+		name = "Debug test (go.mod)",
+		request = "launch",
+		mode = "test",
+		program = "./${relativeFileDirname}",
+	},
+	{
+		type = "go",
+		name = "Debug Current CMD/main.go",
+		request = "launch",
+		program = "./cmd/app/main.go",
+	},
+  {
+		type = "go",
+		name = "Attach to Running Delve",
+		request = "attach",
+		mode = "remote",
+		port = 40000, -- Change this to match the port used by your running dlv instance
+		host = "127.0.0.1",
+	},
+}
